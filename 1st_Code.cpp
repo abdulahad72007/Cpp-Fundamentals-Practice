5c5fb1e772a6fd82360f934e5f629274f5036e70
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-12 18:19:55 +0500

    Add notes on C++ Program Structure - Basic Welcome Ahad Program

---
+++ 1st_Code.cpp
@@ -0,0 +1,7 @@
+#include <iostream>                  // Allows input/output operations
+using namespace std;                // Use standard namespace
+
+int main() {                         // main () funtion is the starting point of C++ Language 
+    cout << "Welcome Ahad!";        // cout Display message on screen 
+    return 0;                       // return 0 indicates the program ended successfully
+}
