a7e458d1e2734bb3637c04fbddb4762d0a1a3a62
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-12 18:30:17 +0500

    Add comments and variables Examples

---
+++ 3rd_Code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Today I learn about Comments & variables in C++
+/*Also about multi line 
+comments by using this
+forward slash with staric.*/
+
+int main(){                 
+    int sum = 2;                                // Variable 'sum' is declared and initialized with value 2
+    cout << "Any text or word." << sum;           // Display text + value of variable 'sum' together
+    return 0;                                   
+}
