7513b1907b50c250c49c062c2b2c37926b803dd5
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-17 23:48:32 +0500

    Add ambiguity problem and resolution with scope operator

---
+++ 43rd_Code.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Base1 {
+    public:
+        void greet(){
+            cout<<"How are You? "<<endl;
+        }
+};
+
+class Base2 {
+    public:
+        void greet(){
+            cout<<"Kaisay ho? "<<endl;
+        }
+};
+
+class Derived : public Base1, public Base2{
+    int a;
+    public:
+    void greet(){
+        Base1::greet();
+    }
+}; 
+
+class B{
+    public:
+    void say(){
+    cout<<"Where are you? " << endl;
+    }
+};
+
+class D{
+    int a;
+    // D's new say() method will override base class's say() method.
+    public:
+    void say()
+    {
+        cout<<"Ok Good bye! "<< endl;
+    }
+};
+
+int main(){
+    // Ambibuity 1
+    /*
+    Base1 base1obj;
+    Base2 base2obj;
+
+    base1obj.greet();
+    base2obj.greet();
+
+    Derived d;
+    d.greet();
+    */
+    // Ambibuity 2
+
+    B b;
+    b.say();
+
+    D d;
+    d.say();
+
+    return 0;
+}
