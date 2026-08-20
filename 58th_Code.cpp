ff3c39d09f6d63e84d0f033839f700f48d4d12e1
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-21 00:34:16 +0500

    Add abstract base class and pure virtual function notes

---
+++ 58th_Code.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout<<" Abstract Base Class / Pure virtual function"<<endl; 
+    cout<<"Abstract Base class is the Class which contains atleast one Pure virtual function.";
+    return 0;
+}
