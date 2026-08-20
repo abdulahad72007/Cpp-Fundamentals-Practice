a2787cc32068a4cb79de903d12696feade038435
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-21 00:34:17 +0500

    Add templates concept notes

---
+++ 63rd_Code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    ---------- Templates ----------
+        Why use Templates?
+
+        Ans: DRY, Generic Programming
+
+    class -> object
+    Template (Parameterized Classes) -> class
+
+                     class Vector {
+                     int *arr;
+                     int size;
+                     Public:  // Vecint, VecFloat, VecDouble, VecChar
+    };
+
+    ---------- Syntax For Templates ----------
+
+    template <class T>          // T can be int, float, char etc...
+    class vector {
+        T* arr;
+        Public:
+        vector(T* ar){
+        // Code
+        }
+        // and many other menthods
+    };
+
+    int main (){
+    vector<int> myvec (ptr);
+    vector<float> myfvec (ptr);
+    }
+    */
+    return 0;
+}
