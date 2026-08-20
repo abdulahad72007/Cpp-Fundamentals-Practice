4ea054ae0e81881bf513cb5514abeab848b3f64f
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-21 00:34:16 +0500

    Add file I/O introduction notes

---
+++ 59th_Code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// What is File in C++?
+
+/*
+---------- File I/O in C++ ----------
+        ^ ---->    [Files] -----^
+        |                       |
+        Can write file      Can read file
+        |                       |
+        \                       /
+              [ C++ Program ]
+
+
+            [cout<< "Can print on screen using cout."]
+            [Console output Screen/Monitor] 
+            We can give input to C++ Program from keyboard [Console input Keyboard]
+*/
+int main(){
+    
+    return 0;
+}
