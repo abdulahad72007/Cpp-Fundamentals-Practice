b9adb1ae48b8bcce2772a70da5548bb3d8391d89
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-22 01:12:47 +0500

    Add STL introduction notes

---
+++ 69th_Code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    ---------- Introduction to STL(Standard Template Library) ----------
+        STL is a library of Generic Classes and Functions.
+
+        Why use STL:
+        Reuse: Well tested components, time saving.
+        Competitive Programming
+
+        Limited time -> Resize
+                     -> Sort
+                     -> Search
+
+        Components of STL are:
+            i) Containers -> Stores data, uses template classes.
+            ii) Algorithms -> Sorting and Searching Algorithms, uses template functions.
+            iii) Iterators -> Object points to an element in a container, handled just like pointers and connects Algorithm with Containers.
+
+            [1][7][11][18][]
+            <-- ^  ----->
+                | <- iterators moves as instructed by the Algorithm.
+
+        STL is used becuase its a good idea and not to Reinvent the wheel.
+    */
+   
+    return 0;
+}
