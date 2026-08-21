59938b5ee664683fbbdf2ad90a50f4405ff58806
Abdul Ahad <abdulahad72007@gmail.com>
2026-08-22 01:12:49 +0500

    Add sorting algorithm with functor

---
+++ 74th_Code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    // Function Objects (Functor): Function wrapped in a class so that it is available like an object.
+    int arr[] = {1, 4, 9, 6, 5, 3};
+    
+    sort(arr, arr + 6);
+    sort(arr, arr + 6, greater<int>());
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
